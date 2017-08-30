27c932970c3474dec18f5961a5a3b7a74d1b7e44
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-08-30 14:59:53 +0300

    Revert "broken material directive -> component"
    This reverts commit acebc8f83dc5a1d1f20a1ff0389d6f56dd8558d1.

--- front-end/app/directives/dashboard/broken/brokenMaterial.js
+++ front-end/app/directives/dashboard/broken/brokenMaterial.js
@@ -1,42 +1,41 @@
 'use strict'
 
 angular.module('koolikottApp')
-    .component('dopBrokenMaterial', {
+.directive('dopBrokenMaterial',
+function () {
+    return {
         templateUrl: 'directives/dashboard/broken/brokenMaterial.html',
-        controller: dopBrokenMaterialController
-    });
+        controller: ['$scope', '$filter', 'serverCallService', function ($scope, $filter, serverCallService) {
+            $scope = $scope.$parent;
 
-dopBrokenMaterialController.inject = ['$scope', '$filter', 'serverCallService'];
-
-function dopBrokenMaterialController($scope, $filter, serverCallService) {
-    $scope = $scope.$parent;
-
-    function init() {
-        $scope.title = $filter('translate')('BROKEN_MATERIALS');
-        serverCallService.makeGet("rest/material/getBroken", {}, success, fail);
-    }
-
-    function success(data) {
-        if (data) $scope.getItemsSuccess(removeDeletedFromBrokens(data), 'byReportCount', true);
-
-        else fail();
-    }
-
-    function fail() {
-        console.log("Failed to get broken materials")
-    }
-
-    //TODO: This should be done in the back-end
-    function removeDeletedFromBrokens(brokens) {
-        var notDeletedBrokens = [];
-        brokens.forEach(function (item) {
-            if (!item.material.deleted) {
-                notDeletedBrokens.push(item);
+            function init() {
+                $scope.title = $filter('translate')('BROKEN_MATERIALS');
+                serverCallService.makeGet("rest/material/getBroken", {}, success, fail);
             }
-        });
 
-        return notDeletedBrokens;
+            function success(data) {
+                if (data) $scope.getItemsSuccess(removeDeletedFromBrokens(data), 'byReportCount', true);
+
+                else fail();
+            }
+
+            function fail() {
+                console.log("Failed to get broken materials")
+            }
+
+            //TODO: This should be done in the back-end
+            function removeDeletedFromBrokens(brokens) {
+                var notDeletedBrokens = [];
+                brokens.forEach(function (item) {
+                    if (!item.material.deleted) {
+                        notDeletedBrokens.push(item);
+                    }
+                });
+
+                return notDeletedBrokens;
+            }
+
+            init();
+        }]
     }
-
-    init();
-}
+});
