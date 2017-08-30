8bf878bb39e14a0df03dcba52ae1ea0869bdf470
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2017-08-30 15:19:16 +0300

    remove unused code

--- front-end/app/directives/dashboard/broken/brokenMaterial.js
+++ front-end/app/directives/dashboard/broken/brokenMaterial.js
@@ -14,25 +14,12 @@ function () {
             }
 
             function success(data) {
-                if (data) $scope.getItemsSuccess(removeDeletedFromBrokens(data), 'byReportCount', true);
-
+                if (data) $scope.getItemsSuccess(data, 'byReportCount', true);
                 else fail();
             }
 
             function fail() {
                 console.log("Failed to get broken materials")
-            }
-
-            //TODO: This should be done in the back-end
-            function removeDeletedFromBrokens(brokens) {
-                var notDeletedBrokens = [];
-                brokens.forEach(function (item) {
-                    if (!item.material.deleted) {
-                        notDeletedBrokens.push(item);
-                    }
-                });
-
-                return notDeletedBrokens;
             }
 
             init();
